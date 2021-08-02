f2b2b016753b1649ccc2ebaead900587b3208d27
vindru <rajatvindr00@gmail.com>
2021-08-02 17:51:25 +0530

    adding typescript support

--- ios/reactNativeBoilerplateTests/reactNativeBoilerplateTests.m
+++ ios/reactNativeBoilerplateTests/reactNativeBoilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeTypescriptBoilerplateTests : XCTestCase
+@interface reactNativeBoilerplateTests : XCTestCase
 
 @end
 
-@implementation reactNativeTypescriptBoilerplateTests
+@implementation reactNativeBoilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
