3ff39101b5d27ab0c9dde989abd15f95eb74ca74
vindru <rajatvindr00@gmail.com>
2021-08-16 16:36:16 +0530

    removing unnecessary files

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,5 +1,3 @@
-export {default as OrderServiceImpl} from './order/order.service.impl';
 export {default as AuthServiceImpl} from './auth/auth.service.impl';
 
-export {OrderService} from './order/order.service';
 export {AuthService} from './auth/auth.service';
